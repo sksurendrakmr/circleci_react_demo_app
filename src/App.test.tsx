8ea4f5211ca9ed8e6f96a3735b76d49d63fd4b14
sksurendrakmr <sksurendrakmr@gmail.com>
2022-09-06 14:46:36 +0530

    break unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/react app with circleci/i);
+  const linkElement = screen.getByText(/hi/i);
   expect(linkElement).toBeInTheDocument();
 });
